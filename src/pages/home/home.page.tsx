74336c17fcea5550bb2fd8baf9010233de32ea6a
Dante Samael Fernández Lago <dantesamaelfernandezlago@gmail.com>
2023-01-24 16:19:23 -0500

    Change: router and pages content.

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-
 import icon from '../../assets/icons-monkey/full-monkey.webp';
-import './home.component.css';
+import './home.page.css';
 
 function Home() {
   return (
-    <section className="Home">
-        <div>
-          <img src={icon} alt='It is an emoji. It shows a sitting monkey.'/>
-        </div>
-        <div>
-          <h1>This site is under constrution.</h1>
-        </div>
-    </section>
+    <div>
+      <section className="Home">
+          <div>
+            <img src={icon} alt='It is an emoji. It shows a sitting monkey.'/>
+          </div>
+          <div>
+            <h1>This site is under constrution.</h1>
+          </div>
+      </section>
+    </div>
   );
 }
 
