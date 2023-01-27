56bf0d6e78126bdb1301ded075440bfe6c65b470
Dante Samael Fernández Lago <dantesamaelfernandezlago@gmail.com>
2023-01-27 17:38:00 -0500

    Change: project and routing structure.

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,11 @@ import './assets/icons-monkey/hear-monkey.webp';
 import './assets/icons-monkey/speak-monkey.webp';
 
 import Home from './pages/home/home.page';
-import Menu from './pages/menu/menu.page';
-import Cart from './pages/cart/cart.page';
-
-import Navigation from './components/navigation/navigation.component';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='menu' element={<Menu />} />
-      </Route>
+      <Route path='/' element={<Home />}/>
     </Routes>
   )
 }
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,19 +1,41 @@
-import icon from '../../assets/icons-monkey/full-monkey.webp';
 import './home.page.css';
 
-function Home() {
-  return (
-    <div>
-      <section className="Home">
-          <div>
-            <img src={icon} alt='It is an emoji. It shows a sitting monkey.'/>
-          </div>
-          <div>
-            <h1>This site is under constrution.</h1>
-          </div>
-      </section>
-    </div>
-  );
+import { Outlet } from 'react-router-dom';
+import { Center, Grid, GridItem } from '@chakra-ui/react';
+
+const Home = () => {
+	return (
+		<div className='Home'>
+			<Center>
+				<Grid
+					templateAreas={`"header header"
+										"nav main"
+										"nav footer"`}
+					gridTemplateRows={'50px 1fr 30px'}
+					gridTemplateColumns={'150px 1fr'}
+					h='75vh'
+					w='125vh'
+					gap='1'
+					color='blackAlpha.700'
+					fontWeight='bold'
+				>
+					<GridItem pl='2' bg='orange' area={'header'}>
+						Header
+					</GridItem>
+					<GridItem pl='2' bg='pink' area={'nav'}>
+						Nav
+					</GridItem>
+					<GridItem pl='2' bg='green' area={'main'}>
+						Main
+					</GridItem>
+					<GridItem pl='2' bg='blue' area={'footer'}>
+						Footer
+					</GridItem>
+				</Grid>
+			</Center>
+			<Outlet />
+		</div>
+	);
 }
 
 export default Home;
